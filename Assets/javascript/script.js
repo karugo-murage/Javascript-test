bf2d656f290a75b22d98d27b2206c6497bc7e14c
Legitt10 <giddykarugo@gmail.com>
2022-05-14 23:03:21 +0300

    updates js file

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,33 +1,43 @@
 
 function result() {
-    var score=0
+    var mark=0
         if (document.getElementById('answer1').checked){
-                score++;
+                mark++;
         }
         if (document.getElementById('answer2').checked){
-                score++;
+                mark++;
             }
         if (document.getElementById('answer3').checked){
-                score++;
+                mark++;
         }
         if (document.getElementById('answer4').checked){
-                score++;
+                mark++;
         }
         if (document.getElementById('answer5').checked){
-                score+=2;
+                mark+=2;
         }
         if (document.getElementById('answer6').checked){
-                score+=2;
+                mark+=2;
         }
         if (document.getElementById('answer7').checked){
-                score+=2;
+                mark+=2;
         }
         if (document.getElementById('answer8').checked){
-                score+=2;
+                mark+=2;
         }
        
-            alert("Your scored " + score +" out of 8");
+            alert("Your scored " + mark +" out of 12");
+        if(mark<6){
+                alert("You need to redo the test");
+        }
+        else if(mark>=6 && mark<9.6){
+                alert("You fairly passed.");
+        }
+        else{
+                alert("Excellent")
+        }    
 }
 function success(){
-    alert("Confirm if you are ready to start the quiz now");
+    alert("Confirm if you are ready to start the quiz now !");
 }
+
